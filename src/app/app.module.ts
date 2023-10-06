be2a142d739e0a3a22950907aefccabcd9c3c654
EvertuLima <78697248+EvertuLima@users.noreply.github.com>
2023-10-06 17:13:26 -0300

    adição de components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// PAGES-COMPONENTS
+import { LoginComponent } from './components/pages/login/login.component';
+import { NewUserComponent } from './components/pages/new-user/new-user.component';
+import { RoomsComponent } from './components/pages/rooms/rooms.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { RoomIdComponent } from './components/pages/room-id/room-id.component';
+
+// SHARED-COMPONENTS
+import { FilterComponent } from './components/shared/filter/filter.component';
+import { RoomSummaryComponent } from './components/shared/room-summary/room-summary.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NewUserComponent,
+    RoomsComponent,
+    RoomSummaryComponent,
+    RoomIdComponent,
+    FilterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/shared/room-summary/room-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-summary',
+  templateUrl: './room-summary.component.html',
+  styleUrls: ['./room-summary.component.css']
+})
+export class RoomSummaryComponent {
+
+}
