16797c481ee289b2947f880accc92baee94798eb
EvertuLima <evertonlima1630@gmail.com>
2023-12-03 20:06:53 -0300

    Tentando usar o material-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
 
 // SHARED-COMPONENTS
 import { FilterComponent } from './components/shared/filter/filter.component';
@@ -42,7 +43,7 @@ import { CreateEmployeeComponent } from './components/shared/create-employee/cre
     NewUserComponent,
     RoomsComponent,
     RoomSummaryComponent,
-    RoomIdComponent,
+    // RoomIdComponent,
     FilterComponent,
     NavbarComponent,
     AboutUsComponent,
@@ -66,6 +67,7 @@ import { CreateEmployeeComponent } from './components/shared/create-employee/cre
     MatInputModule,
     MatSelectModule,
     MatCardModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
